61e7296b5b556d82159fd6280fb9e7b23d2f3038
bculkin2442 <bjculkin@mix.wvu.edu>
2018-09-16 21:50:48 -0400

    Testing

--- src/test/java/bjc/inflexion/InflectionMLTest.java
+++ src/test/java/bjc/inflexion/InflectionMLTest.java
@@ -11,5 +11,17 @@ public class InflectionMLTest {
 	public void testNumDirective() {
 		assertEquals("no results", inflect("<#n:0> <N:results>"));
 		assertEquals("7 results",  inflect("<#n:7> <N:results>"));
+
+		// FIXME
+		//
+		// Adjust this to use <V> for were/was when it is implemented
+		assertEquals("no items were found", inflect("<#n:0> <N:item> were found"));
+		assertEquals("no item was found", inflect("<#s:0> <N:item> was found"));
+
+		assertEquals("a result", inflect("<#a:1> <N:results>"));
+		assertEquals("3 results", inflect("<#a:3> <N:results>"));
+
+		assertEquals("an outcome", inflect("<#a:1> <N:outcomes>"));
+		assertEquals("3 outcomes", inflect("<#a:3> <N:outcomes>"));
 	}
 }
